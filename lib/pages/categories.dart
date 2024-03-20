77ca64f33de0e2a6ec461183dc05a8c9af027ae7
Ravi Acharya <rockyacharya18@gmail.com>
2024-03-20 17:21:22 +0530

    changes made

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -11,32 +11,117 @@ class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(top: 50),
-      child: Column(
-        children: [
-          Center(
-            child: Text(
-              "Categories",
-              style: TextStyle(color: Colors.black,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Poppins' ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Categories",
+                style: TextStyle(color: Colors.black,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins' ),
+              ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20)
+            SizedBox(height: 20,),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Stack(
+                children: [
+                ClipRRect(
+                 borderRadius: BorderRadius.circular(20),
+                    child: Image.asset("images/Wildlife.jpg", width: MediaQuery.of(context).size.width,height: 180, fit: BoxFit.cover,)),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 180,
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(child: Text("WildLife", style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 28, fontWeight: FontWeight.bold),)),
+                  )
+                ],
+              ),
             ),
-            child: Stack(
-              children: [
-              Image.asset("")
-              ],
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              child: Stack(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset("images/Food.jpg", width: MediaQuery.of(context).size.width,height: 180, fit: BoxFit.cover,)),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 180,
+                    decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(child: Text("Foods", style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 28, fontWeight: FontWeight.bold),)),
+                  )
+                ],
+              ),
             ),
-          )
-        ],
-      ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              child: Stack(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset("images/nature.jpg", width: MediaQuery.of(context).size.width,height: 180, fit: BoxFit.cover,)),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 180,
+                    decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(child: Text("Nature", style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 28, fontWeight: FontWeight.bold),)),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              child: Stack(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset("images/City.jpg", width: MediaQuery.of(context).size.width,height: 180, fit: BoxFit.cover,)),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 180,
+                    decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(child: Text("City", style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 28, fontWeight: FontWeight.bold),)),
+                  )
+                ],
+              ),
+            ),
+
+          ],
+        ),
+        ),
       ),
     );
   }
--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sabmilega/pages/photo_model.dart';
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -8,8 +9,38 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+
+  List<PhotosModel> photos=[];
+  TextEditingController searchcontroller = new TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(children: [
+        Center(
+          child: Text(
+            "Search",
+             style: TextStyle(color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins' ),
+          ),
+        ),
+          SizedBox(height: 20.0,),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 3.0),
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Color(0xFFececf8),
+                borderRadius: BorderRadius.circular(10)),
+        child: TextField(
+          controller: searchcontroller,
+          decoration: InputDecoration(border: InputBorder.none, suffixIcon: Icon(Icons.search_outlined, color: Color.fromARGB(255, 84, 87, 93),)),
+        ),  )
+      ],),),
+    );
   }
 }
